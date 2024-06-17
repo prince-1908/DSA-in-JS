6f9b4b944498d8bf5559abe1764673e155010eb9
Prince Singh Chouhan <princesinghchouhan7470@gmail.com>
2024-06-18 01:10:08 +0530

    shuffle

--- 02 Array/manipulations/shuffle.js
+++ 02 Array/manipulations/shuffle.js
@@ -1,8 +1,13 @@
 let arr = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
 
+// Looping from last to 0th index of the array
 for(let i = arr.length - 1; i >= 0; i--){
+
+    // selecting a random index
     let random = Math.floor(Math.random() * i);
 
+    // Swap
+    // here, we are swapping using destructuring
     [arr[i], arr[random]] = [arr[random], arr[i]]
 }
 
